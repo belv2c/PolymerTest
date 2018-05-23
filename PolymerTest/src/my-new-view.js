980688e3652593eb8c7a89063d19fd3e59f6ea1b
Brooke Lewis <belv2c@gmail.com>
2018-05-22 22:03:22 -0500

    added third party web component

--- PolymerTest/src/my-new-view.js
+++ PolymerTest/src/my-new-view.js
@@ -1,5 +1,6 @@
 /* Load the PolymerElement base class and html helper function */
 import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
+import '@polymer/paper-checkbox/paper-checkbox.js';
 /* Load shared styles. All view elements use these styles */
 import './shared-styles.js';
 
@@ -19,6 +20,7 @@ class MyNewView extends PolymerElement {
       <div class="card">
         <div class="circle">1</div>
         <h1>New View</h1>
+        <paper-checkbox>Ready to deploy!</paper-checkbox>
         <p>New view!</p>
       </div>
     `;
